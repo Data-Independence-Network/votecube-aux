b239fe4aa9b5c741898406dddb31978b43f9acbf
russoturisto <russoturisto@gmail.com>
2020-01-18 20:19:26 -0700

    Went to cron job approach, for the time being (mostly to save on required computing resources and for easier scale upgrades, hopefully).  Eventually we might get back to a long running process.

--- main.go
+++ main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"bytes"
 	"flag"
-	"fmt"
-	"github.com/fasthttp/router"
 	"github.com/gocql/gocql"
 	"github.com/klauspost/compress/gzip"
-	"github.com/robfig/cron"
 	"github.com/scylladb/gocqlx"
 	"github.com/scylladb/gocqlx/qb"
-	"github.com/valyala/fasthttp"
 	"io"
 	"log"
 	"strings"
@@ -76,20 +72,54 @@ type Thread struct {
 	LastProcessedDate string
 }
 
-func AlterConfig(ctx *fasthttp.RequestCtx) {
-	testVar := ctx.UserValue("testVar")
-	fmt.Fprintf(ctx, "Hello, %s!\n", testVar)
-}
-
 func GetDateStamps() (string, string) {
 	now := time.Now()
-	today := now.Format("2006-01-02")
-	yesterday := now.AddDate(0, 0, -1).Format("2006-01-02")
+	today := now.Format("20060102")
+	yesterday := now.AddDate(0, 0, -1).Format("20060102")
 
 	return today, yesterday
 }
 
-func Daily() {
+func main() {
+	flag.Parse()
+
+	// connect to the ScyllaDB cluster
+	cluster = gocql.NewCluster(strings.SplitN(*scdbHosts, ",", -1)...)
+
+	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.RoundRobinHostPolicy())
+	//cluster.Compressor = &gocql.SnappyCompressor{}
+	cluster.RetryPolicy = &gocql.ExponentialBackoffRetryPolicy{NumRetries: 3}
+	cluster.Consistency = gocql.Any
+
+	cluster.Keyspace = "votecube"
+
+	session, err = cluster.CreateSession()
+
+	if err != nil {
+		// unable to connect
+		panic(err)
+	}
+	defer session.Close()
+
+	stmt, names := qb.Select("poll_keys").Columns("poll_id").BypassCache().ToCql()
+	getPollIds = gocqlx.Query(session.Query(stmt), names)
+
+	stmt, names = qb.Select("threads").Columns("data").Where(qb.Eq("poll_id")).BypassCache().ToCql()
+	getThreadData = gocqlx.Query(session.Query(stmt), names)
+
+	stmt, names = qb.Select("opinions").Columns("data", "processed").Where(qb.Eq("poll_id"), qb.Eq("date")).BypassCache().ToCql()
+	getOpinionDataForThread = gocqlx.Query(session.Query(stmt), names)
+
+	stmt, names = qb.Update("threads").Set("data", "last_processed_date").Where(qb.Eq("poll_id")).ToCql()
+	updateThread = gocqlx.Query(session.Query(stmt), names)
+
+	stmt, names = qb.Update("opinions").Set("processed").Where(qb.Eq("poll_id"), qb.Eq("date")).ToCql()
+	updateOpinions = gocqlx.Query(session.Query(stmt), names)
+
+	runJob()
+}
+
+func runJob() {
 	todayStamp, yesterdayStamp = GetDateStamps()
 
 	// TODO: make this code multi-threaded
@@ -217,83 +247,4 @@ threadLoop:
 		}
 	}
 
-}
-
-func main() {
-	flag.Parse()
-
-	// connect to the ScyllaDB cluster
-	cluster = gocql.NewCluster(strings.SplitN(*scdbHosts, ",", -1)...)
-
-	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.RoundRobinHostPolicy())
-	//cluster.Compressor = &gocql.SnappyCompressor{}
-	cluster.RetryPolicy = &gocql.ExponentialBackoffRetryPolicy{NumRetries: 3}
-	cluster.Consistency = gocql.Any
-
-	cluster.Keyspace = "votecube"
-
-	session, err = cluster.CreateSession()
-
-	if err != nil {
-		// unable to connect
-		panic(err)
-	}
-	defer session.Close()
-
-	stmt, names := qb.Select("poll_keys").Columns("poll_id").ToCql()
-	getPollIds = gocqlx.Query(session.Query(stmt), names)
-
-	stmt, names = qb.Select("threads").Columns("data").Where(qb.Eq("poll_id")).ToCql()
-	getThreadData = gocqlx.Query(session.Query(stmt), names)
-
-	stmt, names = qb.Select("opinions").Columns("data", "processed").Where(qb.Eq("poll_id"), qb.Eq("date")).ToCql()
-	getOpinionDataForThread = gocqlx.Query(session.Query(stmt), names)
-
-	stmt, names = qb.Update("threads").Set("data", "last_processed_date").Where(qb.Eq("poll_id")).ToCql()
-	updateThread = gocqlx.Query(session.Query(stmt), names)
-
-	stmt, names = qb.Update("opinions").Set("processed").Where(qb.Eq("poll_id"), qb.Eq("date")).ToCql()
-	updateOpinions = gocqlx.Query(session.Query(stmt), names)
-
-	cron.New(
-		cron.WithLocation(time.UTC)).AddFunc("0 4 * * *", Daily)
-
-	r := router.New()
-	r.GET("/alterConfig", AlterConfig)
-	log.Fatal(fasthttp.ListenAndServe(*addr, r.Handler))
-}
-
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	/*
-		User create request
-		  Verify that the userId is not yet taken in the database
-			Get a new user Seq Id
-			Create new user in ScyllaDB
-			Create a JWT token and send it back to the client
-	*/
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-	//ctx.SetContentType("text/plain; charset=utf8")
-
-	// Set arbitrary headers
-	//ctx.Response.Header.Set("X-My-Header", "my-header-value")
-
-	// Set cookies
-	//var c fasthttp.Cookie
-	//c.SetKey("cookie-name")
-	//c.SetValue("cookie-value")
-	//ctx.Response.Header.SetCookie(&c)
 }
