45047135b3de40e07b13a41958a30bcd31c4794b
russoturisto <russoturisto@gmail.com>
2020-01-19 12:16:38 -0700

    Refactoring.

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module bitbucket.org/votecube/votecube-ui-non-read
 go 1.13
 
 require (
-	github.com/klauspost/compress v1.9.7 // indirect
+	bitbucket.org/votecube/votecube-go-lib v0.0.0
 	github.com/scylladb/gocqlx v1.3.3 // indirect
 )
 
 replace github.com/gocql/gocql => github.com/scylladb/gocql v1.3.1
+
+replace bitbucket.org/votecube/votecube-go-lib v0.0.0 => ../votecube-go-lib
--- main.go
+++ main.go
@@ -1,84 +1,31 @@
 package main
 
 import (
-	"bytes"
+	"bitbucket.org/votecube/votecube-go-lib/model/scylladb"
+	"bitbucket.org/votecube/votecube-go-lib/utils"
 	"flag"
 	"github.com/gocql/gocql"
-	"github.com/klauspost/compress/gzip"
 	"github.com/scylladb/gocqlx"
 	"github.com/scylladb/gocqlx/qb"
-	"io"
 	"log"
 	"strings"
-	"sync"
-	"time"
 )
 
 var (
-	scdbHosts                  = flag.String("scdbHosts", "localhost", "TCP address to listen to")
-	addr                       = flag.String("addr", ":8446", "TCP address to listen to")
-	cluster                    *gocql.ClusterConfig
-	session                    *gocql.Session
-	err                        error
-	todayStamp, yesterdayStamp = GetDateStamps()
-	getOpinionDataForThread    *gocqlx.Queryx
-	getThreadData              *gocqlx.Queryx
-	getPollIds                 *gocqlx.Queryx
-	updateOpinions             *gocqlx.Queryx
-	updateThread               *gocqlx.Queryx
-	// https://blog.klauspost.com/gzip-performance-for-go-webservers/
-	gzippers = sync.Pool{New: func() interface{} {
-		return gzip.NewWriter(nil)
-	}}
-	gunzippers = sync.Pool{New: func() interface{} {
-		reader, _ := gzip.NewReader(nil)
-
-		return reader
-	}}
+	scdbHosts                     = flag.String("scdbHosts", "localhost", "TCP address to listen to")
+	addr                          = flag.String("addr", ":8446", "TCP address to listen to")
+	cluster                       *gocql.ClusterConfig
+	session                       *gocql.Session
+	err                           error
+	currentPeriod, previousPeriod = utils.GetPartitionPeriods()
+	getOpinionDataForThread       *gocqlx.Queryx
+	getThreadData                 *gocqlx.Queryx
+	getPollIds                    *gocqlx.Queryx
+	updateOpinions                *gocqlx.Queryx
+	updateThread                  *gocqlx.Queryx
 
 	//compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
 )
-
-type Opinion struct {
-	OpinionId uint64
-	PollId    uint64
-	Date      string
-	UserId    uint64
-	//CreateDt time.Time
-	CreateEs  int64
-	Data      []byte
-	Processed bool
-}
-
-type Poll struct {
-	PollId   uint64
-	UserId   uint64
-	CreateEs int64
-	Data     []byte
-}
-
-type PollKey struct {
-	PollId   uint64
-	UserId   uint64
-	CreateEs int64
-	BatchId  int
-}
-
-type Thread struct {
-	PollId            uint64
-	UserId            uint64
-	CreateEs          int64
-	Data              []byte
-	LastProcessedDate string
-}
-
-func GetDateStamps() (string, string) {
-	now := time.Now()
-	today := now.Format("20060102")
-	yesterday := now.AddDate(0, 0, -1).Format("20060102")
-
-	return today, yesterday
-}
 
 func main() {
 	flag.Parse()
@@ -89,7 +36,7 @@ func main() {
 	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.RoundRobinHostPolicy())
 	//cluster.Compressor = &gocql.SnappyCompressor{}
 	cluster.RetryPolicy = &gocql.ExponentialBackoffRetryPolicy{NumRetries: 3}
-	cluster.Consistency = gocql.Any
+	cluster.Consistency = gocql.One
 
 	cluster.Keyspace = "votecube"
 
@@ -107,144 +54,111 @@ func main() {
 	stmt, names = qb.Select("threads").Columns("data").Where(qb.Eq("poll_id")).BypassCache().ToCql()
 	getThreadData = gocqlx.Query(session.Query(stmt), names)
 
-	stmt, names = qb.Select("opinions").Columns("data", "processed").Where(qb.Eq("poll_id"), qb.Eq("date")).BypassCache().ToCql()
+	stmt, names = qb.Select("opinions").Columns("data", "insert_processed").Where(qb.Eq("poll_id"), qb.Eq("create_period")).BypassCache().ToCql()
 	getOpinionDataForThread = gocqlx.Query(session.Query(stmt), names)
 
-	stmt, names = qb.Update("threads").Set("data", "last_processed_date").Where(qb.Eq("poll_id")).ToCql()
+	stmt, names = qb.Update("root_opinions").Set("data", "version").Where(qb.Eq("poll_id")).ToCql()
 	updateThread = gocqlx.Query(session.Query(stmt), names)
 
-	stmt, names = qb.Update("opinions").Set("processed").Where(qb.Eq("poll_id"), qb.Eq("date")).ToCql()
+	stmt, names = qb.Update("opinions").Set("insert_processed").Where(qb.Eq("poll_id"), qb.Eq("create_period")).ToCql()
 	updateOpinions = gocqlx.Query(session.Query(stmt), names)
 
 	runJob()
 }
 
 func runJob() {
-	todayStamp, yesterdayStamp = GetDateStamps()
+	currentPeriod, previousPeriod = utils.GetPartitionPeriods()
 
-	// TODO: make this code multi-threaded
-
-	var pollKeys []PollKey
+	var pollKeys []scylladb.PollKey
 
 	if err := getPollIds.Select(&pollKeys); err != nil {
 		log.Fatal(err)
 	}
 
-threadLoop:
+	// TODO: make this code multi-threaded once we move off singe-core systems
 	for _, pollKey := range pollKeys {
-		var threads []Thread
-		threadDataQuery := getThreadData.BindMap(qb.M{
-			"poll_id": pollKey.PollId,
-		})
-		if err := threadDataQuery.Select(&threads); err != nil {
-			log.Print(err)
-			continue
-		}
+		processThread(pollKey)
+	}
+}
 
-		previousCompressedData := threads[0].Data
-		var buf bytes.Buffer
-
-		// Get a Writer from the Pool
-		// https://blog.klauspost.com/gzip-performance-for-go-webservers/
-		gz := gzippers.Get().(*gzip.Writer)
-		gz.Reset(&buf)
-
-		if previousCompressedData != nil {
-			threadDataReader := bytes.NewReader(threads[0].Data)
-			gunz := gunzippers.Get().(*gzip.Reader)
-			gunz.Reset(threadDataReader)
-
-			if _, err := io.Copy(gz, gunz); err != nil {
-				log.Print(err)
-
-				gunz.Close()
-				gunzippers.Put(gunz)
-				gz.Close()
-				gzippers.Put(gz)
-
-				continue
-			}
-
-			gunz.Close()
-			gunzippers.Put(gunz)
-		}
-
-		var opinions []Opinion
-		opinionDataQuery := getOpinionDataForThread.BindMap(qb.M{
-			"poll_id": pollKey.PollId,
-			"date":    yesterdayStamp,
-		})
-		if err := opinionDataQuery.Select(&opinions); err != nil {
-			log.Print(err)
-			continue
-		}
-
-		prefixComma := previousCompressedData != nil
-		numUnprocessedOpinions := 0
-		for _, opinion := range opinions {
-			if opinion.Processed {
-				continue
-			}
-
-			opinionDataReader := bytes.NewReader(opinion.Data)
-			gunz := gunzippers.Get().(*gzip.Reader)
-			gunz.Reset(opinionDataReader)
-
-			if prefixComma {
-				gz.Write([]byte(","))
-			}
-			prefixComma = true
-
-			if _, err := io.Copy(gz, gunz); err != nil {
-				log.Print(err)
-
-				gunz.Close()
-				gunzippers.Put(gunz)
-				gz.Close()
-				gzippers.Put(gz)
-
-				continue threadLoop
-			}
-
-			gunz.Close()
-			gunzippers.Put(gunz)
-
-			numUnprocessedOpinions++
-		}
-
-		gz.Close()
-		// When done, put the Writer back in to the Pool
-		gzippers.Put(gz)
-
-		if numUnprocessedOpinions == 0 {
-			continue
-		}
-
-		thread := Thread{
-			PollId:            pollKey.PollId,
-			Data:              buf.Bytes(),
-			LastProcessedDate: yesterdayStamp,
-		}
-
-		updateThreadCommand := updateThread.BindStruct(thread)
-		if err := updateThreadCommand.Exec(); err != nil {
-			log.Print(err)
-
-			continue
-		}
-
-		opinion := Opinion{
-			PollId:    pollKey.PollId,
-			Date:      yesterdayStamp,
-			Processed: true,
-		}
-
-		updateOpinionsCommand := updateOpinions.BindStruct(opinion)
-		if err := updateOpinionsCommand.Exec(); err != nil {
-			log.Print(err)
-
-			continue
-		}
+func processThread(
+	pollKey scylladb.PollKey,
+) bool {
+	var threads []scylladb.Thread
+	threadDataQuery := getThreadData.BindMap(qb.M{
+		"poll_id": pollKey.PollId,
+	})
+	if err := threadDataQuery.Select(&threads); err != nil {
+		log.Print(err)
+		return false
 	}
 
+	previousCompressedData := threads[0].Data
+
+	buf, gz, ok := utils.UnzipToNewZipper(previousCompressedData)
+	if !ok {
+		return false
+	}
+	defer utils.CloseZipper(gz)
+
+	var opinions []scylladb.Opinion
+	opinionDataQuery := getOpinionDataForThread.BindMap(qb.M{
+		"poll_id": pollKey.PollId,
+		"date":    previousPeriod,
+	})
+	if err := opinionDataQuery.Select(&opinions); err != nil {
+		log.Print(err)
+		return false
+	}
+
+	prefixComma := previousCompressedData != nil
+	numUnprocessedOpinions := 0
+	for _, opinion := range opinions {
+		if opinion.InsertProcessed {
+			continue
+		}
+
+		if prefixComma {
+			gz.Write([]byte(","))
+		}
+		prefixComma = true
+
+		if !utils.UnzipToZipper(opinion.Data, gz) {
+			return false
+		}
+
+		numUnprocessedOpinions++
+	}
+
+	if numUnprocessedOpinions == 0 {
+		return true
+	}
+
+	thread := scylladb.Thread{
+		PollId: pollKey.PollId,
+		Data:   buf.Bytes(),
+		//LastProcessedDate: previousPeriod,
+	}
+
+	updateThreadCommand := updateThread.BindStruct(thread)
+	if err := updateThreadCommand.Exec(); err != nil {
+		log.Print(err)
+
+		return false
+	}
+
+	opinion := scylladb.Opinion{
+		PollId:          pollKey.PollId,
+		CreatePeriod:    previousPeriod,
+		InsertProcessed: true,
+	}
+
+	updateOpinionsCommand := updateOpinions.BindStruct(opinion)
+	if err := updateOpinionsCommand.Exec(); err != nil {
+		log.Print(err)
+
+		return false
+	}
+
+	return true
 }
